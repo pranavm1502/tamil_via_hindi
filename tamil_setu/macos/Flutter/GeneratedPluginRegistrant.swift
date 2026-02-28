f0ae0ed11e8d0c7c14e34778d2156dfd37417a28
Pranav Mundada <pranavm1502@gmail.com>
2026-02-28 05:00:02 -0800

    upgrade dependencies

--- tamil_setu/macos/Flutter/GeneratedPluginRegistrant.swift
+++ tamil_setu/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -12,7 +12,6 @@ import firebase_core
 import flutter_local_notifications
 import flutter_tts
 import google_sign_in_ios
-import path_provider_foundation
 import screen_retriever_macos
 import shared_preferences_foundation
 import window_manager
@@ -25,7 +24,6 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
   FlutterTtsPlugin.register(with: registry.registrar(forPlugin: "FlutterTtsPlugin"))
   FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
-  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   ScreenRetrieverMacosPlugin.register(with: registry.registrar(forPlugin: "ScreenRetrieverMacosPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   WindowManagerPlugin.register(with: registry.registrar(forPlugin: "WindowManagerPlugin"))
